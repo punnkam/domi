999727420dfccf0d3421f96c49cb332823c4db89
Peter Nguyen <peternguyenforwork@gmail.com>
2023-02-18 13:59:49 -0800

    Added a dummy profile page

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -1,15 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
+import { Avatar } from "@rneui/themed";
+import UserProfileDummy from "../assets/user-profile-dummy.png";
 
 export default function ProfileScreen() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        flexDirection: "column",
         alignItems: "center",
+        paddingVertical: 50,
       }}
     >
-      <Text>Profile!</Text>
+      <Avatar size={80} rounded source={UserProfileDummy} />
+      <Text style={{ fontWeight: "bold", fontSize: 32, marginVertical: 5 }}>
+        Landlord name
+      </Text>
+      <Text style={{ color: "gray", fontSize: 24, marginVertical: 5 }}>
+        3 PROPERTIES
+      </Text>
+      <View style={{ width: "80%" }}>
+        <InputField label={"Email"} value={"landlord@gmail.com"} />
+        <InputField label={"Phone"} value={"(000) 000-0000"} />
+      </View>
+    </View>
+  );
+}
+
+function InputField({ label, value }) {
+  return (
+    <View
+      style={{
+        borderWidth: 2,
+        borderColor: "gray",
+        borderStyle: "solid",
+        borderRadius: 7,
+        padding: 10,
+        marginVertical: 10,
+        width: "100%",
+      }}
+    >
+      <Text style={{ color: "gray", fontSize: 14 }}>{label}</Text>
+      <TextInput value={value} />
     </View>
   );
 }
