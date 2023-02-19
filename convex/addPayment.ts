6b256de987b748405fb25be4e06b9571ef0cbeff
Punn Kam <punn@euler.xyz>
2023-02-19 06:29:04 -0800

    fixes

--- convex/addPayment.ts
+++ convex/addPayment.ts
@@ -3,6 +3,12 @@ import { Payment } from '../utils/types';
 import { PaymentStatus } from '../utils/types';
 
 export default mutation(async ({ db }, from, to, amount, purpose) => {
+    if (from === to) {
+        throw new Error('Cannot make a payment to yourself');
+    } else if (amount <= 0) {
+        throw new Error('Cannot make a payment of $0 or less');
+    }
+
     const payment: Payment = {
         from: from,
         to: to,
