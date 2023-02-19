60cceac8cf11901522fcce70dbb999518174cf8f
Peter Nguyen <peternguyenforwork@gmail.com>
2023-02-19 01:29:00 -0800

    convex action stuff

--- convex/actions/createPaymentIntent.ts
+++ convex/actions/createPaymentIntent.ts
@@ -1,7 +1,8 @@
-import fetch from "node-fetch";
 import { action } from "../_generated/server";
 
 // Post a GIF chat message corresponding to the query string.
 export default action(async ({ runMutation }, queryString, author) => {
-  return { queryString, author };
+  console.log("queryString = ", queryString, ", author = ", author);
+  // return { queryString, author };
+  await runMutation("addProperty", "abc", "xyz", "123", "456", "7", "8", "9");
 });
